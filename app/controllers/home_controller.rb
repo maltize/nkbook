4de439e92a3ed6bd5bdd5752eca2565d1a20cd50
Maciej <maltize@gmail.com>
2010-10-08 21:55:19 +0200

    refactore home controller

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -13,12 +13,24 @@ class HomeController < ApplicationController
     nBody = Nokogiri::HTML(page.body)
     @photo_url = nBody.xpath("//a[@class = 'card_photo']/img").first.values.first
 
-    # file_path = RAILS_ROOT + '/public/assets/' + @nk_id + '.jpg'
-    file_path = RAILS_ROOT + '/tmp/' + @nk_id + '.jpg'
+    # base_path = RAILS_ROOT + '/public/assets/'
+    base_path = RAILS_ROOT + '/tmp/assets/'
+    file_path = base_path + @nk_id + '.jpg'
+    Dir.mkdir(base_path) unless File.directory?(base_path)
     unless File.exists?(file_path)
       image = Mechanize.new.get_file @photo_url
       open(file_path, "wb") do |file|
         file.write(image)
+      end
+    end
+
+    ImageScience.with_image(file_path) do |img|
+      img.cropped_thumbnail(39) do |thumb|
+        thumb.save base_path + @nk_id + '_cropped.png'
+      end
+
+      img.thumbnail(39) do |thumb|
+        thumb.save base_path + @nk_id + '_thumb.png'
       end
     end
   end
--- config/environment.rb
+++ config/environment.rb
@@ -22,6 +22,8 @@ Rails::Initializer.run do |config|
 
   config.gem "mechanize", :version => '>=1.0.0'
   config.gem "nokogiri", :version => '>=1.4.3.1'
+  config.gem "RubyInline", :version => '>=3.8.6', :lib => "inline"
+  config.gem "image_science", :version => '>=1.2.1'
 
   # Only load the plugins named here, in the order given (default is alphabetical).
   # :all can be used as a placeholder for all plugins not explicitly named
