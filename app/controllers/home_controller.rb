8c1ebb291cc9d5fe318048eaa1e03179dffc3402
Maciej <maltize@gmail.com>
2010-10-08 20:37:36 +0200

    changes

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -13,7 +13,8 @@ class HomeController < ApplicationController
     nBody = Nokogiri::HTML(page.body)
     @photo_url = nBody.xpath("//a[@class = 'card_photo']/img").first.values.first
 
-    file_path = RAILS_ROOT + '/public/assets/' + @nk_id + '.jpg'
+    # file_path = RAILS_ROOT + '/public/assets/' + @nk_id + '.jpg'
+    file_path = RAILS_ROOT + '/tmp/' + @nk_id + '.jpg'
     unless File.exists?(file_path)
       image = Mechanize.new.get_file @photo_url
       open(file_path, "wb") do |file|
