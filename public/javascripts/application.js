6c7636e7d0d029a7364579d5b6410a2a5ec844d8
Maciej <maltize@gmail.com>
2010-10-14 15:10:11 +0200

    updated js

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -18,3 +18,27 @@ $(document).ready(function(){
   $("div.widget_observer").hoverIntent( configHover );
 
 });
+
+function evalScript(scriptStr) {
+  var anonymous_div = $('<div/>');
+  anonymous_div.html(decodeURIComponent(scriptStr));
+
+  anonymous_div.find('script').each(function(el) {
+    var script = document.createElement('script');
+    script.src = $(this).attr('src');
+    document.body.appendChild(script);
+    this.parentNode.removeChild(this);
+    debugger;
+  });
+}
+
+document.write = (function () {
+  var content = "";
+  for (var i = 0; i < arguments.length; i++) {
+    //console.log(arguments[i]);
+    content += arguments[i];
+    //$('div#thawte').append(arguments[i]);
+  }
+  console.log(content);
+  $(this).parentNode.html(content);
+});
